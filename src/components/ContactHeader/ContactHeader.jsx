aca30be9797a97c86d25b86ddd3a7259fe4916bb
tirtharaj21 <rumakarmakar747@gmail.com>
2024-01-27 18:43:11 +0530

    updated commit

--- src/components/ContactHeader/ContactHeader.jsx
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -7,7 +7,7 @@ const ContactHeader = () => {
         <i>Contact Us</i>
       </h1>
       <p>
-      We are here to help you,friends. Whether you have a question, comment or you just want to chat you with us. you can connect with us through the contact form of this page or you can connect with us by Phone, Email or social media handles.
+      We are here to help you,friends. Whether you have a question, comment or you just want to chat with us. you can connect with us through the contact form of this page or you can connect with us by Phone, Email or social media handles.
       Our form is open 24/7 to assist you in all possible ways.  
         </p>
     </div>
